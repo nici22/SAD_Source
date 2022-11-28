91dae27e39063bc54c6c2e3d66123f2a77ff6586
nici22 <ehmedovnic@gmail.com>
2022-11-28 10:49:39 +0400

    sss

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true,
 }));
 app.use(express.json({ limit: '50mb' }));
