cca27fcdffbe79c526880123ede4bbf314cb18a4
nici22 <ehmedovnic@gmail.com>
2022-11-28 10:04:51 +0400

    aa

--- index.js
+++ index.js
@@ -32,7 +32,9 @@ app.use('/api/posts', postRoute);
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static("client/build"));
     app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"client","build","index.html"));
+        res.sendFile(path.join(__dirname,"index.html"));
+//        res.sendFile(path.join(__dirname,"client","build","index.html"));
+
     });
 }
 
